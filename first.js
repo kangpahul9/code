78152079b80013b58ce84e0a7962a70e531f28c4
kangpahul9 <pahulpreet2959@gmail.com>
2024-09-20 13:22:24 +0930

    ABC

---
+++ first.js
@@ -0,0 +1,23 @@
+console.log("Pahul is a Billionaire by 2028");
+console.log("I will surely achieve it");
+age = 24;
+price = 99.99;
+fullname = "Tony Stark";
+radius = 14;
+x=null;
+y= undefined;
+console.log(fullname);
+console.log(age);
+console.log(price);
+console.log(x);
+console.log(y);
+console.log(radius);
+isnotFollow=false;
+console.log(isnotFollow);
+isFollow=true;
+fullname=25;
+console.log(fullname);
+
+console.log(isFollow);
+
+
