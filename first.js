32960795131c51dd5fd59437bf117b81ad8325ae
kangpahul9 <pahulpreet2959@gmail.com>
2024-09-20 17:16:22 +0930

    ABC

--- first.js
+++ first.js
@@ -1,4 +1,4 @@
-console.log("Pahul is a Billionaire by 2028");
+/*console.log("Pahul is a Billionaire by 2028");
 console.log("I will surely achieve it");
 age = 24;
 price = 99.99;
@@ -18,6 +18,77 @@ isFollow=true;
 fullname=25;
 console.log(fullname);
 
-console.log(isFollow);
+console.log(isFollow);*/
+
+
+const student = {
+    fullname:"Rahul Kumar",
+    age :20,
+    cgpa : 8.2,
+    isPass:true
+};
+student["age"]=student["age"]+1;
+console.log(student["age"]);
+student["name"]="Rahul Sharma";
+console.log(student["name "]);
+
+const product = {
+    title: "Ball pen",
+    rating:4,
+    offer:5,
+    price:270
+};
+console.log(product);
+
+const profile = {
+    username:"@kangpahul9",
+    isFollow:false,
+    message:true,
+    posts:190+" Posts",
+    followers:19.5+"K Followers",
+    followers:19.5+" Following",
+    name:"Pahul Preet Singh",
+    bio:"Bathinda X Australia🇦🇺 "
+};
+console.log(profile);
+
+a=5;
+b=7;
+console.log("a+b = ",a+b);
+console.log("a-b = ",a-b);
+console.log("a*b = ",a*b);
+console.log("a/b = ",a/b);
+console.log("a%b = ",a%b);
+console.log("a**b = ",a**b);
+
+a++;
+console.log("a = ",a);
+a--;
+a--;
+console.log("a = ",a);
+a++
+console.log("++a = ",++a);
+a--;
+console.log("a++ = ",a++);
+console.log("a = ",a);
+
+
+let age = 15;
+if (age>18){
+    console.log("you can vote");
+}
+else{
+    console.log("you cannot vote");
+
+}
+mode="dark";
+let color;
+if (mode==="dark"){
+    color="dark";}
+if(mode==="light"){
+color= "light";
+}
+console.log(color)
+
 
 
