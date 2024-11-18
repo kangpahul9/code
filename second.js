9d19001bad6637a6acf3d0bfe4a4dae8e7c8618b
kangpahul9 <pahulpreet2959@gmail.com>
2024-11-18 19:05:02 +1030

    a

--- second.js
+++ second.js
@@ -1,192 +1,252 @@
-{a = 5;
-b = 4;
-c=5;
-d="5";
-console.log("a==b",a==b)
-console.log("a!=b",a!=b)
-console.log("a>c",a>c)
-console.log("a>b",a>b)
-console.log("a<b",a<b)
-console.log("a>=b",a>=b)
-console.log("a<=b",a<=b)
-console.log("a==c",a==c)
-console.log("a==d",a==d)
-console.log("a===c",a===c)
-console.log("a===d",a===d)
-console.log("a!==c",a!==c)
-console.log("a!==d",a!==d)
-}
-// logical operators
- a =6;
- b =4;
-let cond1 = a>b;
-let cond2 = a===6;
-console.log(" a>b && a===6 = ",cond1&&cond2)
-
-console.log(" a===b || a===6 = ",a===b||cond2)
-console.log("!(6===5)",!(a===5)) 
-
-// conditional operators
-age=19;
-if(age>=18){
-    console.log("You can vote")
-}
-if(age<18){
-    console.log("You cannot  vote")
-}
-
-number=10;
-if(number%2===0)
-{
-    console.log(number,' is an even number')
-}
-else{
-    console.log(number,' is an odd number')
-}
-
-// syntax = rules 
-
-
-let mode ="dark";let colour;
-if(mode==="dark")
-{
-    colour="Black";
-}
-else if(mode==="blue"){
-    colour="Blue";
-}
-else if(mode==="Pink")
-{
-    colour="Pink";
-
-}
-else{
-    colour="White";
-}
-console.log(colour) 
-if(mode==="dark") console.log(mode)
-
-let result = age >= 18 ? "Adult" : "not adult";
-console.log(result)
-ge = 16
-ge >= 18 ? console.log("Adult") : console.log("not adult"); //simpler or compact if-else
-
-//switch
-
-const exp = "Papayas";
-switch(exp){
-    case 'Mangoes':
-        console.log("Mangoes are $10.99 each");
-        break;
-    case 'Papayas':
-    case 'Mangoes':
-        console.log("Mangoes and Papayas are $5.99 per kg");
-        break;
-    default:
-        console.log("Sorry we are out of ",exp)
-}
-// const foo = 0;
-// switch (foo) {
-//   case -1:
-//     console.log("negative 1");
-//     break;
-//   case 0: // Value of foo matches this criteria; execution starts from here
-//     console.log(0);
-//   // Forgotten break! Execution falls through
-//   case 1: // no break statement in 'case 0:' so this case will run as well
-//     console.log(1);
-//     break; // Break encountered; will not continue into 'case 2:'
-//   case 2:
-//     console.log(2);
-//     break;
-//   default:
-//     console.log("default");
+// {a = 5;
+// b = 4;
+// c=5;
+// d="5";
+// console.log("a==b",a==b)
+// console.log("a!=b",a!=b)
+// console.log("a>c",a>c)
+// console.log("a>b",a>b)
+// console.log("a<b",a<b)
+// console.log("a>=b",a>=b)
+// console.log("a<=b",a<=b)
+// console.log("a==c",a==c)
+// console.log("a==d",a==d)
+// console.log("a===c",a===c)
+// console.log("a===d",a===d)
+// console.log("a!==c",a!==c)
+// console.log("a!==d",a!==d)
 // }
-// // Logs 0 and 1
+// // logical operators
+//  a =6;
+//  b =4;
+// let cond1 = a>b;
+// let cond2 = a===6;
+// console.log(" a>b && a===6 = ",cond1&&cond2)
 
-// /*Take an input*/
-// // alert("Hello")  //one time pop up
-// let numb = prompt("Enter a Number :")
-// if(numb%5 ===0){
-//     console.log(numb, " is a multiple of 5")
-// }else{
-//     console.log(numb, " is not a multiple of 5")
+// console.log(" a===b || a===6 = ",a===b||cond2)
+// console.log("!(6===5)",!(a===5)) 
+
+// // conditional operators
+// age=19;
+// if(age>=18){
+//     console.log("You can vote")
+// }
+// if(age<18){
+//     console.log("You cannot  vote")
 // }
 
-
-// /*Give grades */
-// grade = prompt("Enter your marks :");
-// if(grade>=90 && grade <= 100  )
-//     {
-//         console.log("At ",grade," marks you got grade 'A'")
-//     }
-// else if(grade>=70 && grade <90)
-//     {
-//         console.log("At ",grade," marks you got grade 'B'")
-//     }
-// else if(grade>=60 && grade < 70)
-//     {
-//         console.log("At ",grade," marks you got grade 'C'");
-//     }
-// else if(grade>=50 && grade <60)
+// number=10;
+// if(number%2===0)
 // {
-//     console.log("At ",grade," marks you got grade 'D'")
+//     console.log(number,' is an even number')
 // }
 // else{
-//     console.log("At ",grade," marks you got grade 'F'")
-
+//     console.log(number,' is an odd number')
 // }
 
+// // syntax = rules 
 
 
-
-// loops
-// for(let i = 1; i<= 5; i ++ ){
-//     console.log("PAHUL PREET SINGH")//5 executions 
+// let mode ="dark";let colour;
+// if(mode==="dark")
+// {
+//     colour="Black";
 // }
-// let sum=0;
-// for(let count = 0; count <= 10; count++){
-//     sum+=count;
+// else if(mode==="blue"){
+//     colour="Blue";
 // }
-// console.log(sum)
-let size=0;
-str="UNI ADELAIDE"
-for(let i of str) {
-    console.log("i= ", i)  
-    size++; 
-}
-console.log("string size ", size)
+// else if(mode==="Pink")
+// {
+//     colour="Pink";
 
-const student = {
-    fullname:"Rahul Kumar",
-    age :20,
-    cgpa : 8.2,
-    isPass:true
-};
-for(let key in student){
-    console.log("key = ",key, "value = ",student[key] )
-}
+// }
+// else{
+//     colour="White";
+// }
+// console.log(colour) 
+// if(mode==="dark") console.log(mode)
 
-n=0;
-while(n<=100)
+// let result = age >= 18 ? "Adult" : "not adult";
+// console.log(result)
+// ge = 16
+// ge >= 18 ? console.log("Adult") : console.log("not adult"); //simpler or compact if-else
+
+// //switch
+
+// const exp = "Papayas";
+// switch(exp){
+//     case 'Mangoes':
+//         console.log("Mangoes are $10.99 each");
+//         break;
+//     case 'Papayas':
+//     case 'Mangoes':
+//         console.log("Mangoes and Papayas are $5.99 per kg");
+//         break;
+//     default:
+//         console.log("Sorry we are out of ",exp)
+// }
+// // const foo = 0;
+// // switch (foo) {
+// //   case -1:
+// //     console.log("negative 1");
+// //     break;
+// //   case 0: // Value of foo matches this criteria; execution starts from here
+// //     console.log(0);
+// //   // Forgotten break! Execution falls through
+// //   case 1: // no break statement in 'case 0:' so this case will run as well
+// //     console.log(1);
+// //     break; // Break encountered; will not continue into 'case 2:'
+// //   case 2:
+// //     console.log(2);
+// //     break;
+// //   default:
+// //     console.log("default");
+// // }
+// // // Logs 0 and 1
+
+// // /*Take an input*/
+// // // alert("Hello")  //one time pop up
+// // let numb = prompt("Enter a Number :")
+// // if(numb%5 ===0){
+// //     console.log(numb, " is a multiple of 5")
+// // }else{
+// //     console.log(numb, " is not a multiple of 5")
+// // }
+
+
+// // /*Give grades */
+// // grade = prompt("Enter your marks :");
+// // if(grade>=90 && grade <= 100  )
+// //     {
+// //         console.log("At ",grade," marks you got grade 'A'")
+// //     }
+// // else if(grade>=70 && grade <90)
+// //     {
+// //         console.log("At ",grade," marks you got grade 'B'")
+// //     }
+// // else if(grade>=60 && grade < 70)
+// //     {
+// //         console.log("At ",grade," marks you got grade 'C'");
+// //     }
+// // else if(grade>=50 && grade <60)
+// // {
+// //     console.log("At ",grade," marks you got grade 'D'")
+// // }
+// // else{
+// //     console.log("At ",grade," marks you got grade 'F'")
+
+// // }
+
+
+
+
+// // loops
+// // for(let i = 1; i<= 5; i ++ ){
+// //     console.log("PAHUL PREET SINGH")//5 executions 
+// // }
+// // let sum=0;
+// // for(let count = 0; count <= 10; count++){
+// //     sum+=count;
+// // }
+// // console.log(sum)
+// let size=0;
+// str="UNI ADELAIDE"
+// for(let i of str) {
+//     console.log("i= ", i)  
+//     size++; 
+// }
+// console.log("string size ", size)
+
+// const student = {
+//     fullname:"Rahul Kumar",
+//     age :20,
+//     cgpa : 8.2,
+//     isPass:true
+// };
+// for(let key in student){
+//     console.log("key = ",key, "value = ",student[key] )
+// }
+
+// n=0;
+// while(n<=100)
+// {
+//     if(n%2===0){
+//     console.log(n);}
+//     n++;
+// }
+
+// // let gameno = 24
+// // for(i=0;i<3;i++){
+// //     guess=prompt("Guess the number: ");
+// //     if(guess==gameno){
+// //         console.log("You guessed the correct number, ", gameno)
+// //         break;v 
+// //     }
+// //     else if(i===2 && guess!=gameno)
+// //     {
+// //         console.log("Hard Luck, the correct number was ",gameno)
+// //     }
+// //     else{
+// //         console.log("You're wrong. Please try Again")
+// //     }
+// // }
+
+
+// let strin = "uni adelaide";
+// console.log(strin[0])
+
+// // template literals 
+
+// let obj = { 
+//     item : "Pen",
+//     price : 10
+// };
+
+// let output = `The cost of ${obj.item} is ${obj.price} rupees`;
+// console.log(output);
+
+// let specialstring = `This is a template literal ${1+2+3}`;
+// console.log(specialstring)
+
+// console.log("Apna \n College")
+// console.log("Apna \t College")
+
+// str = "         Apna\tCollege        "
+// console.log(str.length )
+// console.log(str.trim() )//remove whitespace from starting and ending   
+// str_low=str.toLowerCase()
+// console.log(str)
+// console.log(str_low)
+// str=str.toUpperCase()
+// console.log(str)
+
+let marks = [55,66,45,99,98]
+let addd= 0,avg_marks=0; 
+for(mark of marks)
 {
-    if(n%2===0){
-    console.log(n);}
-    n++;
-}
+    addd+=mark;
+ }
+avg_marks=addd / marks.length;
+console.log(`The average is ${avg_marks} marks`)
 
-let gameno = 24
-for(i=0;i<3;i++){
-    guess=prompt("Guess the number: ");
-    if(guess==gameno){
-        console.log("You guessed the correct number, ", gameno)
-        break;
-    }
-    else if(i===2 && guess!=gameno)
-    {
-        console.log("Hard Luck, the correct number was ",gameno)
-    }
-    else{
-        console.log("You're wrong. Please try Again")
-    }
+
+let items = [250,645,300,900,50]
+console.log(items)
+let i =0;
+for(let val of items){
+    let offer = val/10
+    items[i]-=offer
+    i++;
 }
+console.log(items)
+
+
+let companies = ["Bloomberg","Microsoft","Uber", "Google","IBM","Netflix"]
+console.log(companies)
+companies.shift();
+console.log(companies)
+companies = ["Bloomberg","Microsoft","Uber", "Google","IBM","Netflix"]
+companies.splice(2,1,"Ola")
+console.log(companies)
+companies.push("Amazon")
+console.log(companies)
